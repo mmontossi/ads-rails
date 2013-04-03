467635b53840faa27bfc8eda506d31e0e6512869
Mathías Montossi <mmontossi@mattways.com>
2013-04-03 16:28:05 -0300

    Remove tasks folder

--- lib/ads/rails/helpers.rb
+++ lib/ads/rails/helpers.rb
@@ -5,13 +5,13 @@ module Ads
       def google_adsense_include_tag(*args)
         if ::Rails.env.production?
           options = args.extract_options!
-          params = [
-            "google_ad_client = '#{options[:client]}'", 
-            "google_ad_slot = '#{options[:slot]}'", 
-            "google_ad_width = #{options[:width]}", 
-            "google_ad_height = #{options[:height]}"
-          ]
-          content_tag(:script, params.join('; ').html_safe, :type => 'text/javascript') +
+          script = <<-SCRIPT
+            google_ad_client = '#{options[:client]}';
+            google_ad_slot = '#{options[:slot]}'; 
+            google_ad_width = #{options[:width]};
+            google_ad_height = #{options[:height]};
+          SCRIPT
+          content_tag(:script, script.html_safe, :type => 'text/javascript') +
           content_tag(:script, nil, :type => 'text/javascript', :src => 'http://pagead2.googlesyndication.com/pagead/show_ads.js')
         end
       end
