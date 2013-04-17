bde58c9a579872f1404a55466c47870de1a40436
Mathías Montossi <mmontossi@mattways.com>
2013-04-17 18:47:24 -0300

    Fix indentation

--- lib/ads/rails/action_view/base.rb
+++ lib/ads/rails/action_view/base.rb
@@ -6,13 +6,13 @@ module Ads
         def google_adsense_include_tag(*args)
           if ::Rails.env.production?
             options = args.extract_options!
-            script = <<-SCRIPT
+            script = <<-SCRIPT.strip_heredoc
               google_ad_client = '#{options[:client]}';
               google_ad_slot = '#{options[:slot]}'; 
               google_ad_width = #{options[:width]};
               google_ad_height = #{options[:height]};
             SCRIPT
-            content_tag(:script, script.html_safe, :type => 'text/javascript') +
+            content_tag(:script, "\n#{script}".html_safe, :type => 'text/javascript') +"\n"+
             content_tag(:script, nil, :type => 'text/javascript', :src => 'http://pagead2.googlesyndication.com/pagead/show_ads.js')
           end
         end
