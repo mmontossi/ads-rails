cb15b1764a7894b4b0a997a390bfb3585c79a5fa
Mathías Montossi <mmontossi@mattways.com>
2013-10-21 08:58:07 -0200

    Better renderer logic, mantain request protocol and update docs

--- lib/ads/rails/action_view/base.rb
+++ lib/ads/rails/action_view/base.rb
@@ -5,7 +5,9 @@ module Ads
 
         def google_adsense_include_tag(*args)
           options = args.extract_options!
-          if ['production','test'].include? ::Rails.env
+          if ::Rails.application.config.ads.renderer.is_a? Proc
+            instance_exec(options, &::Rails.application.config.ads.renderer)
+          elsif ::Rails.env.production?
             script = <<-SCRIPT
               google_ad_client = '#{options[:client]}';
               google_ad_slot = '#{options[:slot]}'; 
@@ -13,13 +15,9 @@ module Ads
               google_ad_height = #{options[:height]};
             SCRIPT
             content_tag(:script, script.html_safe, type: 'text/javascript') +
-            content_tag(:script, nil, type: 'text/javascript', src: 'http://pagead2.googlesyndication.com/pagead/show_ads.js')
-          elsif ::Rails.env.development?
-            if ::Rails.application.config.ads.renderer.is_a? Proc
-              instance_exec(options, &::Rails.application.config.ads.renderer)
-            else
-              content_tag(:div, nil, style: "width:#{options[:width]}px;height:#{options[:height]}px;background:#c8c8c8;")
-            end
+            content_tag(:script, nil, type: 'text/javascript', src: "#{request.protocol}pagead2.googlesyndication.com/pagead/show_ads.js")
+          else
+            content_tag(:div, nil, style: "width:#{options[:width]}px;height:#{options[:height]}px;background:#c8c8c8;")
           end
         end
 
--- test/include_tag_test.rb
+++ test/include_tag_test.rb
@@ -5,7 +5,6 @@ class IncludeTagTest < ActionDispatch::IntegrationTest
   test "should show include tag" do
     get '/'
     assert_response :success
-    assert response.body.include?("google_ad_client = 'client'")
   end
 
 end
