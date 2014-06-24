40f7b163a977cf4cb16f678ee331cefbc8bfb5a0
Mathías Montossi <mmontossi@museways.com>
2014-06-24 19:39:51 -0300

    Improve readability

--- lib/ads/rails/action_view/base.rb
+++ lib/ads/rails/action_view/base.rb
@@ -12,12 +12,25 @@ module Ads
                 script << "google_ad_#{key} = #{value};\n"
               end
             end
-            content_tag(:script, script.html_safe, type: 'text/javascript') +
-            content_tag(:script, nil, type: 'text/javascript', src: "#{request.protocol}pagead2.googlesyndication.com/pagead/show_ads.js")
+            content_tag(
+              :script,
+              script.html_safe,
+              type: 'text/javascript'
+            ) +
+            content_tag(
+              :script,
+              nil,
+              type: 'text/javascript',
+              src: "#{request.protocol}pagead2.googlesyndication.com/pagead/show_ads.js"
+            )
           elsif ::Rails.application.config.ads.renderer.is_a? Proc
             instance_exec options, &::Rails.application.config.ads.renderer
           else
-            content_tag :div, nil, style: "width:#{options[:width]}px;height:#{options[:height]}px;background:#c8c8c8;"
+            content_tag(
+              :div,
+              nil,
+              style: "width:#{options[:width]}px;height:#{options[:height]}px;background:#c8c8c8;"
+            )
           end
         end
 
